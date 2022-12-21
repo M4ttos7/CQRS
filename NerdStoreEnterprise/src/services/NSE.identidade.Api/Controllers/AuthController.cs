678503513c23ab06935576eb8f8a67ec097deca5
davi <davi.mattosc@gmail.com>
2022-12-21 17:36:01 -0300

    Teste

---
+++ NerdStoreEnterprise/src/services/NSE.identidade.Api/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identidade.API.Models;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [Route("api/identidade")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost] ("nova-conta")]
+        public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = usuarioResgistro.Email,
+                Email = usuarioRegistgro.Email,
+                EmailConfirmed = true
+            };
+
+            var results = await _userManager.CreateAsync(User, usuarioRegistro.Senha);
+
+            if (result.Succeeded)
+            {
+                await _SignInmanager. SignInAsync(user, isPeesistent:false)
+                return Ok();  
+            }
+
+            return BadRequest();    
+        }
+
+        [HttpPost("autenticar")]
+        public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result = await _signInManager.PasswordSigInAsync(usuaruioLogin.Email, UsuarioLogin.Senha,
+                false, true);  
+            
+        if (result.Sucessed) 
+            {
+                return Ok();
+            }
+
+            return BadRequest();        
+
+
+        }
+    }
+}
---
+++ NerdStoreEnterprise/src/services/NSE.identidade.Api/Models/UserViewModels.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NSE.Identidade.API.Models
+{
+    public class UsuarioRegistro
+    {
+        [Required(ErrorMessage = "o campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "o campo {0} está em formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "o campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "o campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Senha { get; set; }
+
+        [Compare("senha", ErrorMessage = "As senhas não conferem.")]
+        public string SenhaConfirmacao { get; set; }
+    }
+
+    public class UsuarioLogin
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    public string Senha { get; set; }
+}
